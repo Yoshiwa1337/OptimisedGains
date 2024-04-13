92bf0636f8bf2489fa64072b676448ea44aae788
Yoshi <forbes.y2403@gmail.com>
2024-04-13 14:48:28 +0100

    starting suggestion project

---
+++ backend/suggestions.php
@@ -0,0 +1,25 @@
+
+<?php
+    $existingExercises = array("Strength", "Hypertrophy", "Flexibility", "Endurance");
+
+    if (isset($_POST['suggestion'])){
+        $name = $_POST['suggestion'];
+
+        if (!empty($name)){
+            foreach ($existingNames as $existingName){
+                if (strpos($existingName, $name) !== false){
+                    echo $existingName;
+                    echo "<br>";
+
+                }
+
+            }
+
+
+        }
+
+    }
+
+
+
+?>
