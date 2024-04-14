cafd46e380740fce6c5091a3bea74058b4adf014
Yoshi <forbes.y2403@gmail.com>
2024-04-14 13:03:52 +0100

    fixed signup css link

--- signup.php
+++ signup.php
@@ -29,7 +29,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../OptimisedGains/css/log-signup.css">
+    <link rel="stylesheet" href="../OptimisedGains/css/signup.css">
     <link rel="stylesheet" href="../OptimisedGains/css/navbar.css">
     <script src="https://kit.fontawesome.com/4ff0141430.js" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
