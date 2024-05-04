a9dff72f017c9a1e520a9d18e9db960a4f98d3c5
Yoshi <forbes.y2403@gmail.com>
2024-05-04 13:16:14 +0100

    gitignore.

--- backend/mailfunc.inc.php
+++ backend/mailfunc.inc.php
@@ -26,12 +26,12 @@ function sendWelcome($userName, $userEmail){
         //Server settings
         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->Host       = '';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'optimisedgains.info@gmail.com';                     //SMTP username
-        $mail->Password   = 'lfpcpwvefwyxinho';                               //SMTP password
-        $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
-        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        $mail->Username   = '';                     //SMTP username
+        $mail->Password   = '';                               //SMTP password
+        $mail->SMTPSecure = '';            //Enable implicit TLS encryption
+        $mail->Port       = ;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
         $mail->setFrom('optimisedgains.info@gmail.com', 'OptimisedGains');
