92bac89b296311dabd2d084f674565687657cc97
Yoshi <forbes.y2403@gmail.com>
2024-05-09 16:06:59 +0100

    Users can only post 1 review.

--- backend/review.inc.php
+++ backend/review.inc.php
@@ -9,29 +9,40 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $userid = $_SESSION['userid'];
 
     $sql2 = "SELECT users_name FROM `users` WHERE users_id = '$userid';";
-    print_r($sql2);
+    //print_r($sql2);
 
     $result2 = mysqli_query($conn, $sql2);
-
 
     if(!$result2){
         echo "User not found";
     }
 
+    $sql3 = "SELECT author FROM `usersreview` WHERE users_id = '$userid';";
+
+    $result3 = mysqli_query($conn, $sql3);
+
     while($row = mysqli_fetch_assoc($result2)){
         $name = $row['users_name'];
     }
 
-    $sql3  = "INSERT INTO `usersreview`(author, message) VALUES('$name', '$reviewMsg')";
+    if(!$check = mysqli_fetch_assoc($result3)){
 
-    $result3 = mysqli_query($conn, $sql3);
+        $sql4  = "INSERT INTO `usersreview`(author, message, users_id) VALUES('$name', '$reviewMsg', '$userid')";
 
-    if($result3){
-        echo "Review inserted successfully";
+        $result4 = mysqli_query($conn, $sql4);
+
+        if($result4){
+            echo "Review inserted successfully";
+        }
+        else{
+            echo "Table not inserted successfully";
+        }
+
     }
     else{
-        echo "Table not inserted successfully";
+        echo "Error: Cannot upload more than one review!";
     }
+
 
 }
 else{
